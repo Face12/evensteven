2dfc573f8d7a690856fa5b3fc7315d27afde831d
Face12 <lolakil@europe.com>
2024-05-10 12:18:16 +0200

    Initial commit

---
+++ src/main/java/se/face/evensteven/EvenstevenApplication.java
@@ -0,0 +1,13 @@
+package se.face.evensteven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenstevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenstevenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/face/evensteven/EvenstevenApplicationTests.java
@@ -0,0 +1,13 @@
+package se.face.evensteven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenstevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
